e8ce0a2c18afdd5ef5281c1246fcc02a2ad1d74d
Nadezhda Repina <repinanr@yandex.ru>
2021-08-27 14:23:54 +0300

    feat: Add task1 solution

--- QuickstartMVNProject/src/test/java/org/example/AppTest.java
+++ QuickstartMVNProject/src/test/java/org/example/AppTest.java
@@ -1,20 +1,43 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    WebDriver driver;
+    WebDriverWait wait;
+    String expectedEmail = "repinanr@yandex.ru";
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void testEnterEmailInLoginInput()
     {
-        assertTrue( true );
+        driver.get("https://crm.geekbrains.space/");
+
+        WebElement emailField = driver.findElement(By.name("_username"));
+        emailField.sendKeys(expectedEmail);
+        String resultString = emailField.getAttribute("value");
+        assertEquals(expectedEmail, resultString);
     }
 }
